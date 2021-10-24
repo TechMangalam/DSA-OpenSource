76127aa42d5256d9e37201760ac3b988d1a86f81
AnjaneshDubey <72691576+AnjaneshDubey@users.noreply.github.com>
2021-10-24 15:18:28 +0530

    Create xpattern.java

---
+++ xpattern.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        
+        int n= scn.nextInt();
+        
+        int r=n;
+        int l=1;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {   
+                if(j==l || j==r)
+                System.out.print("*	");
+                
+                else
+                System.out.print("	");
+            }
+            
+            l++;
+            r--;
+            
+            
+            
+            
+            System.out.println();
+        }
+
+    }
+}
