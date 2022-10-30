3ce4c4ffba87055bb7de0c9a620fbf9890a6e88c
Rizwan9451 <107796407+Rizwan9451@users.noreply.github.com>
2022-10-30 11:11:21 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,11 @@
+class Cube
+{
+    static void main(int x)
+    {
+        if(x<10)
+        {
+            System.out.print("Cube="+x*x*x);
+        }
+        System.out.print("Square="+x*x);
+    }
+}
