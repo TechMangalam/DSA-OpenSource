5d4606abc3de23d59a5472175a5c95f3299264fc
PRVansh <104479256+PRVansh@users.noreply.github.com>
2022-10-30 10:39:24 +0530

    Add files via upload

---
+++ smallest.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class smallest {
+    public static void main(String args[]) {
+        Scanner sn = new Scanner(System.in);
+        int size, k;
+        System.out.print("Enter Size:");
+        size = sn.nextInt();
+        System.out.print("Enter Kth element:");
+        k = sn.nextInt();
+        int a[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter Element:");
+            a[i] = sn.nextInt();
+        }
+        for (int i = 0; i < size - 1; i++)
+            for (int j = 0; j < size - i - 1; j++)
+                if (a[j] > a[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+                System.out.print("After Swap:");
+                for (int i = 0; i < size; i++) {
+                    
+                    System.out.print(a[i]);
+                }
+                System.out.println();
+        System.out.println(a[k-1]);
+    }
+}
