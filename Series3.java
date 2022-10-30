705ffc360d78345f55cc5533d2a9373caee2b222
Rizwan9451 <107796407+Rizwan9451@users.noreply.github.com>
2022-10-30 12:09:45 +0530

    Add files via upload

---
+++ Series3.java
@@ -0,0 +1,12 @@
+class Series3
+{
+    static void main()
+    {
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println(i*2-1+"/"+(i*2));
+            i++;
+        }
+    }
+}
