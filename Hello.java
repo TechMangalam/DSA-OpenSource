24635f6c205d57c94bfda61d9c5af5f48877c534
abhyudaydubey4 <116032708+abhyudaydubey4@users.noreply.github.com>
2023-10-08 10:52:22 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String args[]){
-        System.out.println("Hello World");
+        System.out.println("Hello World"); //The most interesting and wonderful code in this world.
     }
 }
